64cb54ec88cb6e4fe6ed1bdffbde19c85124b3c1
boredguy404 <257398859+boredguy404@users.noreply.github.com>
2026-08-23 02:55:23 -0400

    simplify pocket archive navigation chrome

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='homebase-v76';
+const CACHE='homebase-v77';
 const SHELL=[
   '/', '/index.html', '/manifest.webmanifest', '/assets/icons/homebase-icon.svg',
   '/pages/arcade.html', '/pages/discover.html', '/pages/files.html', '/pages/apps.html',
