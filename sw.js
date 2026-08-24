500af40b2aa21b501273bdb4b8317046ce9c188e
boredguy404 <257398859+boredguy404@users.noreply.github.com>
2026-08-23 20:58:22 -0400

    harden idle runtime and active window focus

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='homebase-v99';
+const CACHE='homebase-v100';
 const SHELL=[
   '/', '/index.html', '/offline.html', '/manifest.webmanifest', '/assets/icons/homebase-icon.svg',
   '/pages/arcade.html', '/pages/discover.html', '/pages/files.html', '/pages/apps.html',
