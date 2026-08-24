0a0c4bf6a9ab1ecb97a22ef8bb5afd71a3b2c154
boredguy404 <257398859+boredguy404@users.noreply.github.com>
2026-08-23 21:08:56 -0400

    simplify first run setup flow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='homebase-v102';
+const CACHE='homebase-v103';
 const SHELL=[
   '/', '/index.html', '/offline.html', '/manifest.webmanifest', '/assets/icons/homebase-icon.svg',
   '/pages/arcade.html', '/pages/discover.html', '/pages/files.html', '/pages/apps.html',
