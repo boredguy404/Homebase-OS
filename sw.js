0bae056d8bde7758e503ec703c55f6fa7fc657ba
boredguy404 <257398859+boredguy404@users.noreply.github.com>
2026-08-23 23:26:58 -0400

    feat: expand weather station live conditions

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='homebase-v113';
+const CACHE='homebase-v114';
 const SHELL=[
   '/', '/index.html', '/offline.html', '/manifest.webmanifest', '/assets/icons/homebase-icon.svg',
   '/pages/arcade.html', '/pages/discover.html', '/pages/files.html', '/pages/apps.html', '/pages/source-connection.html',
