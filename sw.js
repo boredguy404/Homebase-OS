495c2a6b514eb2386bb1a7911b83cf8e6c560074
boredguy404 <257398859+boredguy404@users.noreply.github.com>
2026-08-23 22:07:07 -0400

    remove retired dancer runtime

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='homebase-v105';
+const CACHE='homebase-v106';
 const SHELL=[
   '/', '/index.html', '/offline.html', '/manifest.webmanifest', '/assets/icons/homebase-icon.svg',
   '/pages/arcade.html', '/pages/discover.html', '/pages/files.html', '/pages/apps.html',
